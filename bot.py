93b01e1d1f308021ceed7afdade4505b5789440e
hayper <Freshj@25yandex.ru>
2020-08-12 21:42:40 +0500

    correction list index out of range

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ class BotHandler:
         if len(get_result) > 0:
             last_update = get_result[-1]
         else:
-            last_update = get_result[len(get_result)]
+            last_update = None
             
         return last_update
 
@@ -93,6 +93,9 @@ def main():
         greet_bot.get_updates(new_offset)
 
         last_update = greet_bot.get_last_update()
+        
+        if last_update == None:
+            continue
 
         last_update_id = last_update['update_id']
         last_chat_text = last_update['message']['text']
